360a986931537a12821882e730c18f9f5e6ef1d0
GlanzWen <281616236@qq.com>
2024-11-04 16:20:54 +0800

    DateUtilsInit

---
+++ date/src/main/java/com/glanz/commonutils/date/dateutils/DateUtil.java
@@ -0,0 +1,35 @@
+package com.glanz.commonutils.date.dateutils;
+
+
+/*
+* dateUtils:
+*
+* */
+public class DateUtil {
+
+    String dateFormat1 = "yyyy-MM-dd";
+    String dateFormat2 = "yyyy-MM-dd HH:mm:ss";
+    String dateFormat3 = "yyyy-MM-dd HH:mm:ss.s";
+    String dateFormat4 = "HH:mm:SS";
+
+
+//String to date, (1)YYYY-MM-DD (2)YYYY-MM-DD HH:MM:SS (3)YYYY-MM-DD HH:MM:SS.s (4) HH:MM:SS
+//long to date, (1)YYYY-MM-DD (2)YYYY-MM-DD HH:MM:SS (3)YYYY-MM-DD HH:MM:SS.s (4) HH:MM:SS
+
+
+//get today date
+
+
+//date to string , long
+
+
+//add day, month, year, second, minute, hour
+
+//subtract day, month, year, second, minute, hour
+
+
+//
+//
+//
+
+}
